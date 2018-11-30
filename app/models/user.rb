ec52092d7161142d835677c62fe634c475cb0e65
カワグチヒロヤ <kawaguchihiroya@kawaguchihiroya-no-iMac.local>
2018-11-30 20:43:54 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true,length:{ maximum: 15 }
+  validates :email, presence: true
+  #validates :email,format: {with: /\A[\w+\-.]+@[a-z\d\-.]+.\/[a-z]+\z/i}
+  has_secure_password
+
+  has_many :topics
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
+  PW_REGEX = /\A(?=.＊?[a-z])(?=.＊?\d)[a-z\d]{8,32}\z/i
+ #validates :password, presence: true, format: { with: PW_REGEX }
+ #validates :password_confirmation, presence: true, format: { with: PW_REGEX }
+end
