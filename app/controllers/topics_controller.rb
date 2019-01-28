a825f855c88c6c5658e422569d38b9001a5d4b4a
kawaguchi <kawaguchihiroya1117@outlook.jp>
2019-01-28 21:24:55 +0900

    first commit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -13,7 +13,7 @@ class TopicsController < ApplicationController
     if @topic.save
       redirect_to topics_path, success: '投稿に成功しました'
     else
-      flash.now[:danger] = "投稿に失敗しました"
+      flash.now[:danger] = '投稿に失敗しました'
       render :new
     end
   end
